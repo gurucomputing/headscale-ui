fc923b55c396cf73ee86bb234647768a00ae2770
Christopher Bisset <chris@gurucomputing.com.au>
2022-07-04 22:45:09 +1000

    encapsulated card chevrons inside button for accessibility

--- src/lib/devices/DeviceCard.svelte
+++ src/lib/devices/DeviceCard.svelte
@@ -7,15 +7,17 @@
 <div in:fade class="card-primary">
 	<div on:click={() => (cardExpanded = !cardExpanded)} class="flex justify-between">
 		<span class="font-bold">{device.id}: {device.name}</span>
-		{#if !cardExpanded}
-			<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline flex-shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-				<path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
-			</svg>
-		{:else}
-			<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline flex-shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-				<path stroke-linecap="round" stroke-linejoin="round" d="M5 15l7-7 7 7" />
-			</svg>
-		{/if}
+		<button>
+			{#if !cardExpanded}
+				<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline flex-shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+					<path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
+				</svg>
+			{:else}
+				<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline flex-shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+					<path stroke-linecap="round" stroke-linejoin="round" d="M5 15l7-7 7 7" />
+				</svg>
+			{/if}
+		</button>
 	</div>
 	{#if cardExpanded}
 		<!-- we put a conditional on the outro transition so page changes do not trigger the animation -->
--- src/lib/index/UserCard.svelte
+++ src/lib/index/UserCard.svelte
@@ -4,18 +4,20 @@
 	let cardExpanded = false;
 </script>
 
-<div in:fade class="card-primary">
-	<div on:click={() => cardExpanded = !cardExpanded} class="flex justify-between">
+<div in:fade class="grid grid-cols-1 divide-y p-2 max-w-screen-lg border-2 mx-4 border-gray-100 rounded-md text-sm text-gray-600 shadow">
+	<div on:click={() => (cardExpanded = !cardExpanded)} class="flex justify-between">
 		<span class="font-bold">{user.id}: {user.name}</span>
-		{#if !cardExpanded}
-			<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline flex-shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-				<path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
-			</svg>
-		{:else}
-			<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline flex-shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-				<path stroke-linecap="round" stroke-linejoin="round" d="M5 15l7-7 7 7" />
-			</svg>
-		{/if}
+		<button>
+			{#if !cardExpanded}
+				<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline flex-shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+					<path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
+				</svg>
+			{:else}
+				<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline flex-shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+					<path stroke-linecap="round" stroke-linejoin="round" d="M5 15l7-7 7 7" />
+				</svg>
+			{/if}
+		</button>
 	</div>
 	{#if cardExpanded}
 		<!-- we put a conditional on the outro transition so page changes do not trigger the animation -->
