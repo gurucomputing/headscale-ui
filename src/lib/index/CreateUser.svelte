29da48c76f7989f72bfd2bcc4fd791b036d1deab
Christopher Bisset <chris@gurucomputing.com.au>
2022-07-05 18:46:38 +1000

    added alert UI, improved createuser ui

--- src/lib/index/CreateUser.svelte
+++ src/lib/index/CreateUser.svelte
@@ -1,19 +1,65 @@
 <script lang="ts">
 	import { fade } from 'svelte/transition';
+	// name for user creation
+	let userName = '';
+	// whether the new card html element is visible
+	let newUserCardVisible = false;
+	// The Form used for validating input
+	let newUserForm: HTMLFormElement;
 
-	function createUser() {}
+	function createUser(): void {
+		let headscaleURL = localStorage.getItem('headscaleURL') || '';
+		let headscaleAPIKey = localStorage.getItem('headscaleAPIKey') || '';
+		let endpointURL = '/api/v1/namespace';
+		if (newUserForm.reportValidity()) {
+			fetch(headscaleURL + endpointURL, {
+				method: 'POST',
+				headers: {
+					Accept: 'application/json',
+					Authorization: `Bearer ${headscaleAPIKey}`
+				},
+				body: JSON.stringify({
+					name: userName.toLowerCase()
+				})
+			})
+				.then((response) => {
+					if (response.ok) {
+						response.json().then((data) => {
+							console.log(data);
+						});
+					} else {
+						response.json().then((data) => {
+							console.error(data);
+						});
+					}
+				})
+				.catch((error) => {
+					console.log(error);
+				});
+		} else {
+		}
+	}
 </script>
 
 <!-- html -->
-<div in:fade class="p-4"><button class="btn-primary bg-teal-700 hover:bg-teal-900 " type="button">+ New User</button></div>
-<div in:fade class="card-pending">
-	<input class="card-input" placeholder="name" />
-	<div>
-    <button><svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 inline rounded-full hover:bg-gray-300" fill="none" viewBox="0 0 24 24" stroke="black" stroke-width="2">
-			<path stroke-linecap="round" stroke-linejoin="round" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-		</svg></button>
-		<button><svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 inline rounded-full hover:bg-gray-300" fill="none" viewBox="0 0 24 24" stroke="black" stroke-width="2">
-			<path stroke-linecap="round" stroke-linejoin="round" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
-		</svg></button>
+<div in:fade class="p-4"><button on:click={() => (newUserCardVisible = true)} class="btn-primary bg-teal-700 hover:bg-teal-900 " type="button">+ New User</button></div>
+{#if newUserCardVisible}
+	<div in:fade out:fade={{ duration: newUserCardVisible ? 0 : 500 }} class="card-pending">
+		<form class="relative" bind:this={newUserForm}>
+			<!-- Input has to be lower case, but we will force lower case on submit -->
+			<input bind:value={userName} class="card-input lowercase" required pattern="[a-zA-Z\-\.]+" placeholder="name" />
+		</form>
+		<div>
+			<button on:click={() => createUser()}
+				><svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 inline rounded-full hover:bg-gray-300" fill="none" viewBox="0 0 24 24" stroke="black" stroke-width="2">
+					<path stroke-linecap="round" stroke-linejoin="round" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+				</svg></button
+			>
+			<button on:click={() => (newUserCardVisible = false)}
+				><svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 inline rounded-full hover:bg-gray-300" fill="none" viewBox="0 0 24 24" stroke="black" stroke-width="2">
+					<path stroke-linecap="round" stroke-linejoin="round" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+				</svg></button
+			>
+		</div>
 	</div>
-</div>
+{/if}
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,7 @@
 <script>
 	import '../app.css';
 	import Nav from '$lib/common/nav.svelte';
+	import Alert from '$lib/common/Alert.svelte'
 </script>
 
 <main class="flex flex-col">
@@ -9,6 +10,7 @@
 		<Nav />
 		<!-- main window -->
 		<div class="flex flex-1 flex-col">
+			<Alert></Alert>
 			<!-- header -->
 			<!-- <div class="flex bg-gray-100 h-12 p-4">Header</div> -->
 			<!-- content -->
