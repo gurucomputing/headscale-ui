9ae9aa80d2e3887fdecb31a4e61b4c19e652e230
Christopher Bisset <chris@gurucomputing.com.au>
2022-07-10 19:26:01 +1000

    fixed link css

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -71,7 +71,7 @@
 	{/if}
 	{#if headscaleAPITest === 'failed'}
 		<div in:fade class="max-w-lg  mx-auto p-4 border-4 text-sm text-base-content shadow-lg text-center">
-			<p>API test did not succeed.<br />Headscale might be down or API settings may need to be set<br />change server settings in the <a href="/settings.html" class="hyperlink">settings</a> page</p>
+			<p>API test did not succeed.<br />Headscale might be down or API settings may need to be set<br />change server settings in the <a href="/settings.html" class="link link-primary">settings</a> page</p>
 		</div>
 	{/if}
 {/if}
