9db27aaa28fd4a6f04359e5f820cf8471cc807dc
Christopher Bisset <chris@gurucomputing.com.au>
2022-08-08 08:58:45 +1000

    moved variable out of github actions

--- docker/production/scripts/1-image-build.sh
+++ docker/production/scripts/1-image-build.sh
@@ -8,5 +8,8 @@ git clone ${PROJECT_URL} ${PROJECT_NAME}
 cd ${PROJECT_NAME}
 npm install
 
+# inject the version number
+sed -i 's/insert-version/${{ steps.gathervars.outputs.VERSION }}/g' ./src/routes/settings.html.svelte
+
 # build the project
 npm run build
