022b6855a8a25a71586f8f86c0c1606995efa5a5
Christopher Bisset <chris@gurucomputing.com.au>
2022-08-08 09:22:47 +1000

    added jq to image, moved build image to alpine

--- docker/production/scripts/1-image-build.sh
+++ docker/production/scripts/1-image-build.sh
@@ -4,12 +4,17 @@ set -x
 #clone the project
 git clone ${PROJECT_URL} ${PROJECT_NAME}
 
+# add dependencies
+# jq for parsing version information
+apk add --no-cache jq
+
 # install the project
 cd ${PROJECT_NAME}
 npm install
 
 # inject the version number
-sed -i 's/insert-version/${{ steps.gathervars.outputs.VERSION }}/g' ./src/routes/settings.html.svelte
+VERSION=$(jq -r '.version' package.json)
+sed -i "s/insert-version/${VERSION}/g" ./src/routes/settings.html.svelte
 
 # build the project
 npm run build
