438639804fdbfcd5cac7c91d52b174890523f7e3
Christopher Bisset <chris@gurucomputing.com.au>
2022-09-10 13:25:02 +1000

    added group view page

---
+++ src/routes/groups.html/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	//
+	// Imports
+	//
+	import { showACLPagesStore } from '$lib/common/stores';
+	import { onMount } from 'svelte';
+	import { fade } from 'svelte/transition';
+
+	// Set to true once component is initialized
+	let componentLoaded = false;
+
+	onMount(async () => {
+		// Display component frontend
+		await new Promise((r) => setTimeout(r, 200));
+		componentLoaded = true;
+	});
+</script>
+
+<body>
+	{#if showACLPagesStore}
+		<div hidden={!componentLoaded} in:fade class="px-4 py-4 w-4/5 max-w-screen-lg">
+				<h1 class="text-2xl bold text-primary">Group View</h1>
+		</div>
+	{/if}
+</body>
