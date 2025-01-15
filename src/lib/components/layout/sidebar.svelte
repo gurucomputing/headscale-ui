fdece32b5e2194ca9eccb43532e96cdb2eb5e00e
Chris Bisset <chris@gurucomputing.com.au>
2025-01-15 15:28:04 +1100

    refactor to html endings to keep consistency with previous version

--- src/lib/components/layout/sidebar.svelte
+++ src/lib/components/layout/sidebar.svelte
@@ -22,14 +22,14 @@
 	</a>
 </li>
 <li>
-	<a href="./users"
+	<a href="./users.html"
 		><svg class="h-8 w-8" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
 			<path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"></path>
 		</svg><span class="text-primary">Users</span></a
 	>
 </li>
 <li>
-	<a href="./settings"
+	<a href="./settings.html"
 		><svg class="h-8 w-8" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
 			<path
 				stroke-linecap="round"
