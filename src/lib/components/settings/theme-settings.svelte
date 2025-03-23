90b015bfdaa6cafe0e416d8c7c34918df53b5e85
Chris Bisset <chris@gurucomputing.com.au>
2025-03-23 03:50:57 +0000

    fix svelecte theming

--- src/lib/components/settings/theme-settings.svelte
+++ src/lib/components/settings/theme-settings.svelte
@@ -1,11 +1,8 @@
 <script lang="ts">
+	import Svelecte from 'svelecte';
 	import {persistentAppSettings } from '$lib/components/common/state.svelte';
 	let themeOptions = ['light', 'dark', 'cupcake', 'bumblebee', 'emerald', 'corporate', 'synthwave', 'retro', 'cyberpunk', 'valentine', 'halloween', 'garden', 'forest', 'aqua', 'lofi', 'pastel', 'fantasy', 'wireframe', 'black', 'luxury', 'dracula', 'cmyk', 'autumn', 'business', 'acid', 'lemonade', 'night', 'coffee', 'winter', 'dim', 'nord', 'sunset'];
 </script>
 
 <h1 class="bold mb-4 mt-4 text-xl text-primary">Application Theme</h1>
-<select bind:value={persistentAppSettings.daisyUITheme} class="select">
-	{#each themeOptions as theme }
-		<option>{theme}</option>		
-	{/each}
-</select>
+<Svelecte name="daisyUITheme" options={themeOptions} bind:value={persistentAppSettings.daisyUITheme} />
