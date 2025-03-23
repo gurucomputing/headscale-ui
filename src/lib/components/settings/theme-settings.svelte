3672dabefec234750386c75236386f54c1a3ba13
Chris Bisset <chris@gurucomputing.com.au>
2025-03-23 00:36:37 +0000

    move themes to separate variable

--- src/lib/components/settings/theme-settings.svelte
+++ src/lib/components/settings/theme-settings.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
 	import { persistentAppSettings } from '$lib/components/common/state.svelte';
 	import Svelecte from 'svelecte';
+	let themeOptions = ['hsui', 'light', 'dark', 'cupcake', 'bumblebee', 'emerald', 'corporate', 'synthwave', 'retro', 'cyberpunk', 'valentine', 'halloween', 'garden', 'forest', 'aqua', 'lofi', 'pastel', 'fantasy', 'wireframe', 'black', 'luxury', 'dracula', 'cmyk', 'autumn', 'business', 'acid', 'lemonade', 'night', 'coffee', 'winter', 'dim', 'nord', 'sunset'];
 </script>
 
 <h1 class="bold mb-4 mt-4 text-xl text-primary">Application Theme</h1>
-<Svelecte name="daisyUITheme" options={['hsui', 'light', 'dark', 'cupcake', 'bumblebee', 'emerald', 'corporate', 'synthwave', 'retro', 'cyberpunk', 'valentine', 'halloween', 'garden', 'forest', 'aqua', 'lofi', 'pastel', 'fantasy', 'wireframe', 'black', 'luxury', 'dracula', 'cmyk', 'autumn', 'business', 'acid', 'lemonade', 'night', 'coffee', 'winter', 'dim', 'nord', 'sunset']} bind:value={persistentAppSettings.daisyUITheme} />
+<Svelecte name="daisyUITheme" options={themeOptions} bind:value={persistentAppSettings.daisyUITheme} />
