f8f9d0fe443a860d7219885209b0f1077b076e6e
Chris Bisset <chris@gurucomputing.com.au>
2025-03-23 00:42:10 +0000

    change rotate color based on time

--- src/lib/components/settings/server-settings.svelte
+++ src/lib/components/settings/server-settings.svelte
@@ -28,7 +28,7 @@
 			<label class="mb-2 block font-bold text-secondary" for="headscaleKey"> Headscale API Key </label>
 			{#if appSettings.apiKeyExpiration != undefined}
 				<button type="button" disabled={rotateButtonDisabled} onclick={rotateAPIKeyClick} class="tooltip" data-tip="{appSettings.apiKeyExpiration} days left. Click to rotate key" aria-label="check time left"
-					><svg data-slot="icon" fill="none" class="-my-3 ml-2 h-5 w-5 stroke-success" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+					><svg data-slot="icon" fill="none" class="-my-3 ml-2 h-5 w-5 {appSettings.apiKeyExpiration >= 30 ? "stroke-success" : "stroke-error"}" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
 						<path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"></path>
 					</svg></button
 				>
