9e7ac0904b4bda8d4afe9f78c9a9df7123104c3e
Chris Bisset <chris@gurucomputing.com.au>
2025-03-23 07:03:43 +0000

    shrink button overlay

--- src/lib/components/layout/navbar.svelte
+++ src/lib/components/layout/navbar.svelte
@@ -9,7 +9,7 @@
 		onclick={() => {
 			appSettings.sidebarDrawerOpen = true;
 		}}
-		class="btn btn-square btn-ghost lg:hidden"
+		class="btn btn-square btn-sm btn-ghost lg:hidden"
 	>
 		<svg xmlns="http://www.w3.org/2000/svg" class="m-1 h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
 	</button>
