38134e0f4b4a3a4b8c59fbf4a9812cc34815252b
Chris <chris@gurucomputing.com.au>
2025-08-31 02:36:32 +0000

    perform login check

--- src/lib/components/layout/auth-functions.svelte
+++ src/lib/components/layout/auth-functions.svelte
@@ -1,19 +1,30 @@
 <script module lang="ts">
-    import { goto } from '$app/navigation';
-    import{ resolve } from '$app/paths';
+	import { goto } from '$app/navigation';
+	import{ resolve } from '$app/paths';
 	import { appSettings } from '../common/state.svelte';
+	
+	export async function checkAuth() {
+		let validLogin = appSettings.pb.authStore.isValid;
 
-    export async function checkAuth() {
-
-		// attempt to authenticate with default credentials and redirect to first registration screen
-		try {
-			let authData = await appSettings.pb.collection("_superusers").authWithPassword('superadmin@breakglass.local', 'firsttimepasswordchangeme');
-			if (authData) {
-                appSettings.pb.authStore.clear();
-				goto(resolve(`/firstregistration.html`));
+		if(validLogin) {
+			try {
+				appSettings.pb.collection("users").authRefresh();
+			} catch (error) {
+				validLogin = false;
 			}
-		} catch (error) {
-			console.log('not authenticated');
 		}
-    }
+		
+		if(!validLogin) {
+			// attempt to authenticate with default credentials and redirect to first registration screen
+			try {
+				let authData = await appSettings.pb.collection("_superusers").authWithPassword('superadmin@breakglass.local', 'firsttimepasswordchangeme');
+				if (authData) {
+					appSettings.pb.authStore.clear();
+					goto(resolve(`/firstregistration.html`));
+				}
+			} catch (error) {
+				goto(resolve(`/login.html`))
+			}
+		}
+	}
 </script>
