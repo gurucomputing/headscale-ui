aaee883d2ef7e482312bb3b6110463d721f7674a
Chris <chris@gurucomputing.com.au>
2025-11-16 10:05:11 +0000

    fix duplicate form id

--- src/lib/components/login/registration.svelte
+++ src/lib/components/login/registration.svelte
@@ -18,40 +18,56 @@
 			<label class="label" for="email">Email</label>
 			<input type="email" bind:value={inputEmail} required class="input" id="email" placeholder="Email" />
 			<p class="label">
-				<span>{#if validEmail}
+				<span
+					>{#if validEmail}
 						<svg in:fade class="stroke-success" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="m4.5 12.75l6 6l9-13.5" /></svg>
 					{:else}
 						<svg in:fade class="stroke-error" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M6 18L18 6M6 6l12 12" /></svg>
-					{/if}</span>
+					{/if}</span
+				>
 				Valid E-mail Address
 			</p>
 			<label class="label" for="password">New Password</label>
 			<input class="input" type="password" bind:value={inputPassword} required id="password" placeholder="Password" />
 			<p class="label">
-				<span>{#if hasOneCapital}
+				<span
+					>{#if hasOneCapital}
 						<svg in:fade class="stroke-success" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="m4.5 12.75l6 6l9-13.5" /></svg>
 					{:else}
 						<svg in:fade class="stroke-error" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M6 18L18 6M6 6l12 12" /></svg>
-					{/if}</span>
+					{/if}</span
+				>
 				At least 1 Capital
 			</p>
-			<p class="label"><span>{#if hasOneNumberOrSymbol}
+			<p class="label">
+				<span
+					>{#if hasOneNumberOrSymbol}
 						<svg in:fade class="stroke-success" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="m4.5 12.75l6 6l9-13.5" /></svg>
 					{:else}
 						<svg in:fade class="stroke-error" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M6 18L18 6M6 6l12 12" /></svg>
-					{/if}</span>
-					At least 1 Number or Symbol</p>
-			<p class="label"><span>{#if hasAtLeastEightCharacters}
+					{/if}</span
+				>
+				At least 1 Number or Symbol
+			</p>
+			<p class="label">
+				<span
+					>{#if hasAtLeastEightCharacters}
 						<svg in:fade class="stroke-success" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="m4.5 12.75l6 6l9-13.5" /></svg>
 					{:else}
 						<svg in:fade class="stroke-error" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M6 18L18 6M6 6l12 12" /></svg>
-					{/if}</span>At least 8 characters</p>
-			<input type="password" bind:value={inputRepeatPassword} required class="input" id="password" placeholder="Repeat Password" />
-			<p class="label"><span>{#if doesPasswordsMatch}
+					{/if}</span
+				>At least 8 characters
+			</p>
+			<input type="password" bind:value={inputRepeatPassword} required class="input" placeholder="Repeat Password" />
+			<p class="label">
+				<span
+					>{#if doesPasswordsMatch}
 						<svg in:fade class="stroke-success" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="m4.5 12.75l6 6l9-13.5" /></svg>
 					{:else}
 						<svg in:fade class="stroke-error" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M6 18L18 6M6 6l12 12" /></svg>
-					{/if}</span>Passwords Match</p>
+					{/if}</span
+				>Passwords Match
+			</p>
 			<button class="btn btn-neutral mt-4" disabled={!isFormValid} type="submit">Register Admin Account</button>
 		</fieldset>
 	</form>
